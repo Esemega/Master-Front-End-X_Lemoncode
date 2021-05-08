126e28386b31624675b7918c8294e260c1a04419
Esemega <sogaral89@gmail.com>
2021-05-08 12:26:24 +0200

    Folder name modified

--- Modulo_1-Layout/Ejercicio_1-Paleta colores dinamica/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('.color-box-text').forEach(element => {
-    element.addEventListener('click', () => {
-        let text = window.getComputedStyle(element,':after')['content'].replace(/['"]+/g, '');
-        navigator.clipboard.writeText(text);
-        window.alert("El siguiente texto se ha copiado en el portapapeles:\n"+text)
-    });
-});
