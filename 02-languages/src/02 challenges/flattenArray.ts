e786ac74d90a0b02d734e83a5321265b3a5c791b
Esemega <sogaral89@gmail.com>
2021-06-02 19:58:17 +0200

    Flatten array done

---
+++ 02-languages/src/02 challenges/flattenArray.ts
@@ -0,0 +1,21 @@
+type NestedArray<T> =  T | Array<NestedArray<T>>;
+
+const numberArray: NestedArray<number> = [1, [2, 3], [[4], [5, 6, [7, 8, [9]]]]];
+const stringArray:NestedArray<string> = ["1", ["2", "3"], [["4"], ["5", "6", ["7", "8", ["9"]]]]];
+const booleanArray:NestedArray<boolean> = [true, [false, [false]], true, [[[true]]]];
+
+const flatArray= <T>(arr: Array<NestedArray<T>>): T[]=> {
+        return arr.reduce(
+        (result: any[], element: Array<NestedArray<T>>) =>
+            Array.isArray(element) 
+                ? result.concat(flatArray(element)) 
+                : result.concat(element)
+        , []
+    )
+}
+
+
+console.log("************** FLATTEN ARRAY *********************");
+console.log(flatArray(numberArray));
+console.log(flatArray(stringArray));
+console.log(flatArray(booleanArray));
