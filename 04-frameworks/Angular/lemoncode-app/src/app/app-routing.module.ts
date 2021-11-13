9fc3e3e9ccb2e567b2db1ba7dd271f8794744c85
Esemega <sogaral89@gmail.com>
2021-11-13 12:51:25 +0100

    Add private link pages to private menu and to routing

--- 04-frameworks/Angular/lemoncode-app/src/app/app-routing.module.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsersComponent } from './users/users.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'users/list', component: UsersComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
