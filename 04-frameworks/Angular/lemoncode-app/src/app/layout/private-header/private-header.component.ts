334a867e4aed8b980102c6f4e515f6c5f7258c8d
Esemega <sogaral89@gmail.com>
2021-11-16 19:56:18 +0100

    Create private header, getUsername from AuthService and logout

--- 04-frameworks/Angular/lemoncode-app/src/app/layout/private-header/private-header.component.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/layout/private-header/private-header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-private-header',
   templateUrl: './private-header.component.html',
-  styleUrls: ['./private-header.component.scss']
+  styleUrls: ['./private-header.component.scss'],
 })
 export class PrivateHeaderComponent implements OnInit {
+  username: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private authService: AuthService) {
+    this.username = this.authService.getUsername();
   }
 
+  ngOnInit(): void {}
+
+  logout() {
+    this.authService.logout().subscribe((x) => this.router.navigate(['/home']));
+  }
 }
