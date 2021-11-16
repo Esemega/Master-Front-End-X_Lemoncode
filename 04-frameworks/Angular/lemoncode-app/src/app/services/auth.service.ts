9d2f00b8feada8890709b8024530d8e25e6ca539
Esemega <sogaral89@gmail.com>
2021-11-16 20:01:40 +0100

    Add logout, isLogged and getUsername methods

--- 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
@@ -1,20 +1,35 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { UserEntity } from '../model/UserEntity';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
   constructor() {}
+  _isLogged = false; //Esta variable se eliminará cuando se acceda al localstorage
 
-  login(user: { username: string; password: string }): boolean {
-    return (
-      user.username === 'master8@lemoncode.net' && user.password === '12345678'
-    );
+  login(user: UserEntity): Observable<boolean> {
+    if (
+      user.username === 'master8@lemoncode.net' &&
+      user.password === '12345678'
+    ) {
+      this._isLogged = true;
+      return of(true);
+    } else {
+      return of(false);
+    }
   }
-  // Validación del formulario????
 
-  // Este servicio debe tener los siguientes métodos:
-  // logout(): void {}
-  // isLogged(): boolean {}
-  // getUsername(): string {}
+  logout(): Observable<boolean> {
+    this._isLogged = false;
+    return of(true);
+  }
+
+  isLogged(): Observable<boolean> {
+    return of(this._isLogged);
+  }
+  getUsername(): string {
+    return 'Sonia Garcia';
+  }
 }
