f691eb9c4b58c9a0d69b4c11d9569818480e98b0
Esemega <sogaral89@gmail.com>
2021-11-20 15:52:43 +0100

    Store user session data to localstorage

--- 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
@@ -1,35 +1,52 @@
 import { Injectable } from '@angular/core';
+import { UserEntity, UserSession } from '../model';
 import { Observable, of } from 'rxjs';
-import { UserEntity } from '../model/UserEntity';
+import { delay } from 'rxjs/operators';
+
+const getToken = (user: UserEntity): string => {
+  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hc3RlcjhAbGVtb25jb2RlLm5ldCIsInBhc3N3b3JkIjoiMTIzNDU2NzgifQ.-k6L5ATX3NeF2bwIIQCldqWo63x7ty2dMe3c4O8-rxs';
+};
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  constructor() {}
-  _isLogged = false; //Esta variable se eliminará cuando se acceda al localstorage
+  private userSession: UserSession;
+
+  constructor() {
+    this.userSession = { username: '', token: '' };
+  }
 
   login(user: UserEntity): Observable<boolean> {
     if (
       user.username === 'master8@lemoncode.net' &&
       user.password === '12345678'
     ) {
-      this._isLogged = true;
+      this.userSession.username = user.username;
+      this.userSession.token = getToken(user);
+      localStorage.setItem('username', this.userSession.username);
+      localStorage.setItem('token', this.userSession.token);
+      return of(true).pipe(delay(2000));
+    } else {
+      return of(false).pipe(delay(2000));
+    }
+  }
+
+  logout(): Observable<boolean> {
+    this.userSession = { username: '', token: '' };
+    localStorage.removeItem('username');
+    localStorage.removeItem('token');
+    return of(true);
+  }
+
+  isLogged(): Observable<boolean> {
+    if (localStorage.getItem('token')) {
       return of(true);
     } else {
       return of(false);
     }
   }
-
-  logout(): Observable<boolean> {
-    this._isLogged = false;
-    return of(true);
-  }
-
-  isLogged(): Observable<boolean> {
-    return of(this._isLogged);
-  }
-  getUsername(): string {
-    return 'Sonia Garcia';
+  getUsername(): string | null {
+    return localStorage.getItem('username');
   }
 }
