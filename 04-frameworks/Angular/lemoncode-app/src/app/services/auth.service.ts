f3eaabc1ed4bbfba8009f9f9e8d1668d53609457
Esemega <sogaral89@gmail.com>
2021-11-20 17:13:38 +0100

    Change getToken function by getUser

--- 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/services/auth.service.ts
@@ -3,8 +3,12 @@ import { UserEntity, UserSession } from '../model';
 import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 
-const getToken = (user: UserEntity): string => {
-  return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hc3RlcjhAbGVtb25jb2RlLm5ldCIsInBhc3N3b3JkIjoiMTIzNDU2NzgifQ.-k6L5ATX3NeF2bwIIQCldqWo63x7ty2dMe3c4O8-rxs';
+const getUser = (user: UserEntity): UserSession => {
+  return {
+    username: 'master8',
+    token:
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im1hc3RlcjhAbGVtb25jb2RlLm5ldCIsInBhc3N3b3JkIjoiMTIzNDU2NzgifQ.-k6L5ATX3NeF2bwIIQCldqWo63x7ty2dMe3c4O8-rxs',
+  };
 };
 
 @Injectable({
@@ -22,8 +26,7 @@ export class AuthService {
       user.username === 'master8@lemoncode.net' &&
       user.password === '12345678'
     ) {
-      this.userSession.username = user.username;
-      this.userSession.token = getToken(user);
+      this.userSession = getUser(user);
       localStorage.setItem('username', this.userSession.username);
       localStorage.setItem('token', this.userSession.token);
       return of(true).pipe(delay(2000));
