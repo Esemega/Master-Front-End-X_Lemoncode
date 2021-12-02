26f01c1d7aa4f8ac161300508ccd9da8b82fb404
Esemega <sogaral89@gmail.com>
2021-12-02 18:19:16 +0100

    Gallery component done

--- 04-frameworks/Angular/lemoncode-app/src/app/gallery/gallery.component.ts
+++ 04-frameworks/Angular/lemoncode-app/src/app/gallery/gallery.component.ts
@@ -1,15 +1,85 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image-service.service';
+import { ImageEntity } from '../model/ImageEntity';
 
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  imageData: Array<ImageEntity> = [];
 
-  constructor() { }
+  selectedImageIndex: number = 0;
+  selectedImageSrc: string;
 
-  ngOnInit(): void {
+  size: number = 500;
+
+  delay: number = 2000;
+  intervalID: number | undefined;
+  isPlaying: boolean = false;
+
+  start: number = 0;
+  end: number = this.start + 3;
+  groupSize: number = 3;
+
+  constructor(private imageService: ImageService) {
+    this.selectedImageSrc = '../../assets/img/001.jpg';
   }
 
+  ngOnInit(): void {
+    this.imageService.getImagesData().subscribe((data) => {
+      this.imageData = data;
+      this.setSelectedImageSrc(this.selectedImageIndex);
+    });
+  }
+
+  setSelectedImageSrc(id: number) {
+    this.selectedImageIndex = id;
+    this.selectedImageSrc = `../../assets/img/${this.imageData[id].src}`;
+  }
+
+  next() {
+    this.selectedImageIndex++;
+    this.setSelectedImageSrc(this.selectedImageIndex);
+  }
+
+  previous() {
+    this.selectedImageIndex--;
+    this.setSelectedImageSrc(this.selectedImageIndex);
+  }
+
+  getBigger() {
+    this.size = this.size + 25;
+  }
+
+  getSmaller() {
+    this.size = this.size - 25;
+  }
+
+  play() {
+    this.intervalID = window.setInterval(() => {
+      if (this.selectedImageIndex === this.imageData.length - 1) {
+        this.selectedImageIndex = -1;
+      }
+      this.setSelectedImageSrc(++this.selectedImageIndex);
+    }, this.delay);
+
+    this.isPlaying = true;
+  }
+
+  stop() {
+    window.clearInterval(this.intervalID);
+    this.isPlaying = false;
+  }
+
+  nextGroup() {
+    this.start += this.groupSize;
+    this.end += this.groupSize;
+  }
+
+  previousGroup() {
+    this.start -= this.groupSize;
+    this.end -= this.groupSize;
+  }
 }
