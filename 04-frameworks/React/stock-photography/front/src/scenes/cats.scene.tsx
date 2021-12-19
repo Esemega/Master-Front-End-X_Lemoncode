db8b471a0c42b0850d832a134a53a6ffde46bbc1
Esemega <sogaral89@gmail.com>
2021-12-19 23:40:01 +0100

    Add scenes

---
+++ 04-frameworks/React/stock-photography/front/src/scenes/cats.scene.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const CatsScene: React.FC = () => {
+  return <h1>Hello from Cats Scene</h1>;
+};
---
+++ 04-frameworks/React/stock-photography/front/src/scenes/monkeys.scene.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const MonkeysScene: React.FC = () => {
+  return <h1>Hello from Mokeys Scene</h1>;
+};
