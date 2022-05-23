7dee994b4c9a9db3318fce1dec5b625ecd5ac1d9
Esemega <sogaral89@gmail.com>
2022-05-23 19:20:50 +0200

    Update scenes

---
+++ 06-rest_api/hotels-app/src/scenes/character-collection.scene.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { AppLayout } from 'layouts';
+// import { HotelCollectionContainer } from 'pods/hotel-collection';
+import { CharacterCollectionContainer } from 'pods/character-collection';
+
+export const CharacterCollectionScene = () => (
+  <AppLayout>
+    <CharacterCollectionContainer />
+  </AppLayout>
+);
