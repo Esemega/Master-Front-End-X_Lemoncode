5860e60f31d9a41f42c562ee5ecd4dc35a32a337
Esemega <sogaral89@gmail.com>
2022-05-24 19:57:18 +0200

    remove hotel interface

--- 06-rest_api/hotels-app/src/pods/character-collection/api/character-collection.api-model.ts
+++ 06-rest_api/hotels-app/src/pods/character-collection/api/character-collection.api-model.ts
@@ -1,41 +1,3 @@
-export interface HotelEntityApi {
-  id: string;
-  type: string;
-  name: string;
-  created: Date;
-  modified: Date;
-  address1: string;
-  airportCode: string;
-  amenityMask: number;
-  city: string;
-  confidenceRating: number;
-  countryCode: string;
-  deepLink: string;
-  highRate: number;
-  hotelId: number;
-  hotelInDestination: boolean;
-  hotelRating: number;
-  location: {
-    latitude: number;
-    longitude: number;
-  };
-  locationDescription: string;
-  lowRate: number;
-  metadata: {
-    path: string;
-  };
-  postalCode: number;
-  propertyCategory: number;
-  proximityDistance: number;
-  proximityUnit: string;
-  rateCurrencyCode: string;
-  shortDescription: string;
-  stateProvinceCode: string;
-  thumbNailUrl: string;
-  tripAdvisorRating: number;
-  tripAdvisorRatingUrl: string;
-}
-
 export interface CharacterEntityApi {
   id: number;
   name: string;
@@ -83,5 +45,5 @@ export interface ApiResponse {
     next: string | null;
     prev: string | null;
   };
-  results: CharacterEntityApi[] | LocationEntityApi[] | EpisodeEntityApi[];
+  results: CharacterEntityApi[];
 }
