99262a8b3ef51f4114e6021abc99611d854777a1
Esemega <sogaral89@gmail.com>
2022-07-04 20:48:09 +0200

    remove hotel import

--- 06-rest_api/rick-and-morty-app/src/scenes/character-collection.scene.tsx
+++ 06-rest_api/rick-and-morty-app/src/scenes/character-collection.scene.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { AppLayout } from 'layouts';
-// import { HotelCollectionContainer } from 'pods/hotel-collection';
 import { CharacterCollectionContainer } from 'pods/character-collection';
 
 export const CharacterCollectionScene = () => (
